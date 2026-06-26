2f7829c1bea857416b30a656e0a619195874cc1c
Dede <surya.adhi.dede@gmail.com>
2026-06-26 07:16:03 +0700

    style: fix formatting issues in root layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -11,8 +10,6 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
-
-
 
 export default function RootLayout({
   children,
